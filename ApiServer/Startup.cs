33799b385179da737f7fa851fb5f7b7610bcebac
Michitaro Naito <m-naito@amlitek.com>
2014-03-09 14:33:43 +0900

    Initial

---
+++ ApiServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApiServer.Startup))]
+namespace ApiServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
